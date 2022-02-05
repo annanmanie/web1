42e858a956930e66b9a77575de1960b9796e21e8
annanmanie <98837124+annanmanie@users.noreply.github.com>
2022-02-05 23:14:19 +0900

    test

--- colors.js
+++ colors.js
@@ -42,6 +42,6 @@ $(document).ready(function()
   $('a').hover(function(){
     $(this).css('color', 'red');
   }, function(){
-    $(this).css('color', 'black');
+    $(this).css('color', 'inherit');
   });
 });
